9a40851bbc0220461a86169df00740eec0da6ffa
Jatin Nagpal <nagpaljatin1411@gmail.com>
2018-10-13 08:29:54 +0530

    Added Code Snippet

---
+++ code_snippet.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ff first
+#define ss second
+using namespace std;
+#define MP make_pair
+#define PB push_back
+#define ll long long
+#define int long long
+#define f(i,x,n) for(int i=x;i<n;i++)
+#define mod 1000000007
+main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    return 0;
+}
